5ff4a9d8b8bd040014fe961ad481cf0c8b54658e
Hafizh Ibnu Syam <aqrashu.asus@gmail.com>
2024-11-19 15:55:50 +0700

    Refactors database configuration for Sequelize integration
    Consolidates database configuration management by exporting the dbConfig object from the central database configuration file.
    
    Updates the Sequelize initialization to use the structured dbConfig, enhancing code clarity and maintainability.
    
    Removes redundant configuration interfaces and a set of hardcoded properties, streamlining the connection logic.

--- app/config/db-connect.ts
+++ app/config/db-connect.ts
@@ -1,54 +1,11 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Dialect } from 'sequelize'
+import dbConfig from '@/config/database'
 
-interface DBConfig {
-  dialect: 'sqlite';
-  storage: string;
-  logging: boolean;
-  pool: {
-    max: number;
-    min: number;
-    acquire: number;
-    idle: number;
-  };
-}
-
-const config: { [key: string]: DBConfig } = {
-  development: {
-    dialect: 'sqlite',
-    storage: './database/database.sqlite',
-    logging: false,
-    pool: {
-      max: 1,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  },
-  test: {
-    dialect: 'sqlite',
-    storage: ':memory:',
-    logging: false,
-    pool: {
-      max: 1,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  },
-  production: {
-    dialect: 'sqlite',
-    storage: './database/database.sqlite',
-    logging: false,
-    pool: {
-      max: 1,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  },
-};
-const env = process.env.NODE_ENV || 'development';
-const dbConfig = config[env];
-const sequelize = new Sequelize(dbConfig);
+const sequelize = new Sequelize({
+  dialect: dbConfig.dialect as Dialect,
+  storage: dbConfig.storage,
+  logging: dbConfig.logging,
+  pool: dbConfig.pool,
+});
 
 export { sequelize };
